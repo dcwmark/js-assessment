1f226777df5e7efc38faf2c439e4c03ca744f95d
Entaro Adun <dcwmark@gmail.com>
2016-09-11 02:25:24 -0400

    async behavior

--- app/async.js
+++ app/async.js
@@ -2,10 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+      return new Promise(function(resolve, reject) {
+          if (value === true || value === 'success') {
+              resolve(value);
+          } else {
+              reject(false);
+          }
+      });
   },
 
   manipulateRemoteData: function(url) {
-
+      var def = $.Deferred();
+      return $.ajax(url).then(function(resp) {
+          var people = resp.people.map( each => each.name);
+          return def.resolve(people.sort());
+      });
   }
 };
